14dbb31ac7191f7939770088930d6fab98979aea
Aniluj <julianvolonterio@gmail.com>
2018-05-29 07:03:07 -0300

    Colisiones y movimiento de enemigo
    Ahora si el jugador colisiona con el enemigo, el juego se cierra.
    El enemigo ahora puede moverse verticalmente.

--- JuegoRepositorioTest1/Juego/Juego.cpp
+++ JuegoRepositorioTest1/Juego/Juego.cpp
@@ -4,24 +4,38 @@
 #include "allegro5/allegro_native_dialog.h"
 
 
+bool Collision(int pX, int pY, int eX, int eY, int pWidth, int pHeight, int eWidth, int eHeight){
+	if (pX + pWidth<eX || pX > eX + eWidth || pY + pHeight < eY || pY > eY + eHeight){
+		return false;
+	}
+	else{
+		return true;
+	}
+}
+
+
 int main(int argc, char **argv) {
 
 ALLEGRO_DISPLAY *display = NULL;
 ALLEGRO_BITMAP *player = NULL;
 ALLEGRO_BITMAP *enemie = NULL;
 
-//enum Direction {UP, DOWN, LEFT, RIGHT};
-
 const float fps = 60.0;
 
 bool done = false;
 bool draw = true;
+
+int displayWidth = 800;
+int displayHeight = 600;
+
 int playerX = 10;
 int playerY = 10;
-//int playerDir = DOWN;
 int playerMovementSpeed = 4;
+
+bool enemieGoingUp = false;
 int enemieX = 400;
-int enemieY = 10;
+int enemieY = 0;
+int enemieMovementSpeed = 5;
 
 if (!al_init()) {
 	return 0;
@@ -31,7 +45,7 @@ if (!al_init_image_addon()) {
 	return 0;
 }
 
-display = al_create_display(800, 600);
+display = al_create_display(displayWidth, displayHeight);
 
 if (!display) {
 	return 0;
@@ -66,10 +80,10 @@ while (!done){
 			break;
 		}
 	}
+
 	if (events.type == ALLEGRO_EVENT_TIMER){
 
 		al_get_keyboard_state(&keystate);
-
 		if (al_key_down(&keystate, ALLEGRO_KEY_DOWN)){
 			playerY += playerMovementSpeed;
 		}
@@ -81,6 +95,25 @@ while (!done){
 		}
 		else if (al_key_down(&keystate, ALLEGRO_KEY_LEFT)) {
 			playerX -= playerMovementSpeed;
+		}
+
+
+		if (Collision(playerX, playerY, enemieX, enemieY, al_get_bitmap_width(player), al_get_bitmap_height(player), al_get_bitmap_width(enemie), al_get_bitmap_height(enemie))){
+			done = true;
+		}
+
+
+		if (!enemieGoingUp){
+			enemieY += enemieMovementSpeed;
+		}
+		if (enemieGoingUp){
+			enemieY -= enemieMovementSpeed;
+		}
+		if (enemieY + al_get_bitmap_height(enemie) >= displayHeight){
+			enemieGoingUp = true;
+		}
+		else if (enemieY <= 0){
+			enemieGoingUp = false;
 		}
 
 		draw = true;
