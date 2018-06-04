be827dec49f6afcf6b51b4e292addfe6b64fa891
Unknown <ale.bros@hotmail.com>
2018-06-04 10:20:41 -0300

    Add Menu .h and .cpp

---
+++ JuegoRepositorioTest1/Juego/Menu.h
@@ -0,0 +1,11 @@
+#ifndef MENU_H
+#define MENU_H
+#include "allegro5/allegro.h"
+#include "allegro5/allegro_image.h"
+#include "allegro5/allegro_native_dialog.h"
+
+class Menu {
+public:
+	void Portada(BITMAP* p);
+};
+#endif // MENU_H
